3a6442ee8c8f22a6e6448ad3cb83a2e6ed43b973
hideo-t <hdxrope@gmail.com>
2026-03-10 11:15:31 +0900

    fix: Rakuten URL correct params f_s1/f_m1/f_d1 format

--- rakuten_travel.py
+++ rakuten_travel.py
@@ -147,10 +147,10 @@ function htSearch_{code}(site){{
   if(site==='rakuten'){{
     url='https://travel.rakuten.co.jp/search/HOTEL/'
       +'?f_area={code}'
-      +'&f_checkin='+ci[0]+'-'+ci[1]+'-'+ci[2]
-      +'&f_checkout='+co[0]+'-'+co[1]+'-'+co[2];
-    var affil='{affil}';
-    if(affil) url='https://hb.afl.rakuten.co.jp/hgc/'+affil+'/?pc='+encodeURIComponent(url)+'&m='+encodeURIComponent(url);
+      +'&f_otona_su='+ad
+      +'&f_s1='+ci[0]+'&f_m1='+ci[1]+'&f_d1='+ci[2]
+      +'&f_s2='+co[0]+'&f_m2='+co[1]+'&f_d2='+co[2]
+      +'&scid=af_pc_etc&sc2id=af_101_0_0';
   }} else if(site==='booking'){{
     url='https://www.booking.com/search.html'
       +'?ss='+encodeURIComponent('{pref_en} Japan')
